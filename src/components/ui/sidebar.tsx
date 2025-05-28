2fc1d3ff62cfc0de9b3e816acf52b03e09dce49f
Harshal Raverkar <harshal.raverkar@gmail.com>
2025-05-28 09:23:42 +0530

    code updated with latest changes

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -1,15 +1,15 @@
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
-import { VariantProps, cva } from "class-variance-authority"
+import { type VariantProps, cva } from "class-variance-authority"
 import { PanelLeft } from "lucide-react"
 
 import { useIsMobile } from "@/hooks/use-mobile"
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import { Separator } from "@/components/ui/separator"
+import { Separator } from "@/components/ui/seprator"
 import { Sheet, SheetContent } from "@/components/ui/sheet"
-import { Skeleton } from "@/components/ui/skeleton"
+import { Skeleton } from "@/components/ui/skaleton"
 import {
   Tooltip,
   TooltipContent,
